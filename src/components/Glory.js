9b2b4a65d5fbea5cc708283739a7ee85a1320740
aswath <aswath.710@gmail.com>
2020-09-24 13:39:30 +0530

    glory_commit

--- src/components/Glory.js
+++ src/components/Glory.js
@@ -4,23 +4,71 @@ import './Glory.css';
 export default class Glory extends React.Component {
   render() {
     return <div>
+     <div class="row">
+  <div class="column">
+    <div class="card">
+      <h1>2</h1><br/>
+      <h6>Civil Services</h6>
+      <h5>I.R.A.S/I.R.S</h5>
+    </div>
+  </div>
+  <div class="column">
+    <div class="card">
+    <h1>600+</h1><br/>
+      <h6>Puducherry goverment services</h6>
+
+    </div>
+  </div>
+  </div>
+        
       <div class="row">
   <div class="timeline">
     <ul>
       <li>
         <div class="timeline-content">
-          
         <div className="box"> <h3>2010</h3></div>
-       
-          <table class="table">
+        <table class="table">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Name <br/> of the<br/> post</th>
+      <th scope="col">No. of the Candidates selected</th>
+     
     </tr>
   </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Deputy Tahsildar</th>
+      <td>3</td>
+      
+    </tr>
+    <tr>
+      <th scope="row">Village Administrative Officer</th>
+      <td>6</td>
+      
+    </tr>
+    <tr>
+     <th scope="row">Village Assistant</th>
+     <td>4</td>
+   </tr>
+   <tr>
+     <th scope="row">Employment Information
+Assistant </th>
+     <td>1</td>
+   </tr>
+   <tr>
+     <th scope="row">School Librarian</th>
+     <td>2</td>
+   </tr>
+   <tr>
+     <th scope="row">Postal Assistant (India Post)</th>
+     <td>1</td>
+   </tr> 
+    <tr>
+     <th scope="row">Store Keeper (JIPMER,
+Puducherry)</th>
+     <td>1</td>
+   </tr>
+  </tbody>
   </table>
         </div>
       </li>
@@ -31,11 +79,14 @@ export default class Glory extends React.Component {
        <table class="table">
 <thead>
  <tr>
-   <th scope="col">#</th>
-   <th scope="col">First</th>
-   <th scope="col">Last</th>
-   <th scope="col">Handle</th>
+ <th scope="col">Name of the post</th>
+      <th scope="col">No. of the Candidates selected</th>
  </tr>
+ <tr>
+     <th scope="row">Sub-Inspector</th>
+     <td>4</td>
+   </tr>
+   
 </thead>
 </table>
         </div>
@@ -47,11 +98,27 @@ export default class Glory extends React.Component {
        <table class="table">
 <thead>
  <tr>
-   <th scope="col">#</th>
-   <th scope="col">First</th>
-   <th scope="col">Last</th>
-   <th scope="col">Handle</th>
+ <th scope="col">Name <br/>of the <br/>post</th>
+      <th scope="col">No. of the Candidates selected</th>
  </tr>
+ <tr>
+     <th scope="row">Upper Division Clerk / Senior
+Clerks</th>
+     <td>30+</td>
+   </tr>
+   <tr>
+     <th scope="row">Lower Division Clerk / Store
+Keeper / Junior Clerk / Typist</th>
+     <td>113+</td>
+   </tr>
+   <tr>
+     <th scope="row">Police Constable </th>
+     <td>56+</td>
+   </tr>
+   <tr>
+     <th scope="row">Fireman</th>
+     <td>9</td>
+   </tr>
 </thead>
 </table>
         </div>
@@ -63,11 +130,54 @@ export default class Glory extends React.Component {
        <table class="table">
 <thead>
  <tr>
-   <th scope="col">#</th>
-   <th scope="col">First</th>
-   <th scope="col">Last</th>
-   <th scope="col">Handle</th>
+ <th scope="col">Name <br/> of the <br/>post</th>
+      <th scope="col">No. of the Candidates selected</th>
  </tr>
+ <tr>
+     <th scope="row">Welfare Inspector </th>
+     <td>3</td>
+   </tr>
+   <tr>
+     <th scope="row">Village Administrative Officer </th>
+     <td>6</td>
+   </tr>
+   <tr>
+     <th scope="row">Assistant Commercial Tax
+Officer</th>
+     <td>2+</td>
+   </tr>
+   <tr>
+     <th scope="row">Lower Division Clerk (JIPMER,
+Puducherry)</th>
+     <td>4</td>
+   </tr>
+   <tr>
+     <th scope="row">Assistant Manager (Puduvai
+BharathiarGrama Bank)</th>
+     <td>1</td>
+   </tr>
+   <tr>
+     <th scope="row">Multi-Tasking Staff (JIPMER,
+Puducherry)</th>
+     <td>3</td>
+   </tr>
+   <tr>
+     <th scope="row">IBPS Clerk</th>
+     <td>1</td>
+   </tr>
+   <tr>
+     <th scope="row">Postal Assistant (India Post)</th>
+     <td>4</td>
+   </tr>
+   <tr>
+     <th scope="row">Junior Purchase Assistant
+(Department of Atomic Energy)</th>
+     <td>1</td>
+   </tr>
+   <tr>
+     <th scope="row">Lieutenant (Indian Army)</th>
+     <td>1</td>
+   </tr>
 </thead>
 </table>
         </div>
@@ -79,12 +189,26 @@ export default class Glory extends React.Component {
        <table class="table">
 <thead>
  <tr>
-   <th scope="col">#</th>
-   <th scope="col">First</th>
-   <th scope="col">Last</th>
-   <th scope="col">Handle</th>
+ <th scope="col">Name <br/>of the post</th>
+      <th scope="col">No. of the Candidates selected</th>
  </tr>
 </thead>
+<tr>
+     <th scope="row">Upper Division Clerk </th>
+     <td>134+</td>
+   </tr>
+   <tr>
+     <th scope="row">IRBn Police </th>
+     <td>98+</td>
+   </tr>
+   <tr>
+     <th scope="row">Home Guard / Woman PC </th>
+     <td >84+</td>
+   </tr>
+   <tr>
+     <th scope="row">Lower Division Clerk (JIPMER) </th>
+     <td>3</td>
+   </tr>
 </table>
         </div>
       </li>
@@ -95,12 +219,22 @@ export default class Glory extends React.Component {
        <table class="table">
 <thead>
  <tr>
-   <th scope="col">#</th>
-   <th scope="col">First</th>
-   <th scope="col">Last</th>
-   <th scope="col">Handle</th>
+ <th scope="col">Name <br/>of the<br/> post</th>
+      <th scope="col">No. of the Candidates selected</th>
  </tr>
 </thead>
+<tr>
+     <th scope="row">Section Officer (VCRC,
+Puducherry) </th>
+     <td>1</td>
+   </tr>
+   <tr>
+     <th scope="row">Civil Services Examination (IAS,
+IPS etc.) –
+Thiru P. SUDHANDIRA BALAN
+Selected as I.R.A.S. </th>
+     <td>1</td>
+   </tr>
 </table>
         </div>
       </li>
@@ -111,12 +245,26 @@ export default class Glory extends React.Component {
        <table class="table">
 <thead>
  <tr>
-   <th scope="col">#</th>
-   <th scope="col">First</th>
-   <th scope="col">Last</th>
-   <th scope="col">Handle</th>
+ <th scope="col">Name of <br/>the <br/> post</th>
+      <th scope="col">No. of the Candidates selected</th>
  </tr>
 </thead>
+<tr>
+     <th scope="row">Civil Services Examination 2017 </th>
+     <td></td>
+   </tr>
+   <tr>
+     <th scope="row">(IAS, IPS etc)</th>
+     <td></td>
+   </tr>
+   <tr>
+     <th scope="row">Thiru P.SUTHANDIRA BALAN </th>
+     <td></td>
+   </tr>
+   <tr>
+     <th scope="row">Selected as I.R.S.(AIR 854) </th>
+     <td></td>
+   </tr>
 </table>
         </div>
       </li>
@@ -127,11 +275,19 @@ export default class Glory extends React.Component {
        <table class="table">
 <thead>
  <tr>
-   <th scope="col">#</th>
-   <th scope="col">First</th>
-   <th scope="col">Last</th>
-   <th scope="col">Handle</th>
+ <th scope="col">Name 
+ <br/>of the <br/>
+ post</th>
+      <th scope="col">No. of the Candidates selected</th>
  </tr>
+ <tr>
+     <th scope="row">Jail Warders(Male/Female) </th>
+     <td>1</td>
+   </tr>
+   <tr>
+     <th scope="row">Assistant Loco Pilot (RRB)</th>
+     <td>1</td>
+   </tr>
 </thead>
 </table>
         </div>
