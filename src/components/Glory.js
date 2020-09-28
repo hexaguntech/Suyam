8c80f38650584e3ce28fed7a5f42b343d0c3c6c4
aswath <aswath.710@gmail.com>
2020-09-29 02:29:08 +0530

    testpush

--- src/components/Glory.js
+++ src/components/Glory.js
@@ -4,13 +4,14 @@ import './Glory.css';
 export default class Glory extends React.Component {
   render() {
     return <div>
+      <div className="hexa">
      <div class="row">
   <div class="column">
     <div class="card">
       <h1>2</h1><br/>
       <h6>Civil Services</h6>
       <h5>I.R.A.S/I.R.S</h5>
-    </div>
+  
   </div>
   <div class="column">
     <div class="card">
@@ -20,7 +21,8 @@ export default class Glory extends React.Component {
     </div>
   </div>
   </div>
-        
+  </div>
+  
       <div class="row">
   <div class="timeline">
     <ul>
@@ -297,6 +299,8 @@ Selected as I.R.A.S. </th>
     </ul>
   </div>
 </div>
+</div>
+
     </div>;
   }
 }
