02081c261cf991cf4cce05388d9b8cf3c38d1a6b
Ajaybarath <Ajaybarath@users.noreply.github.com>
2020-10-16 11:03:51 +0530

    scroll added

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-
 import Main from './components/Main';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div>
       <div>
         <BrowserRouter>
+          <ScrollToTop />
           <Main />
         </BrowserRouter>
       </div>
